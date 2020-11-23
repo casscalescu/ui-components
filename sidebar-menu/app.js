30175f958eb8f4f6a474d1974a3cabcf87619b03
Cassandra Calescu <casscalescu@gmail.com>
2020-11-23 18:26:16 +1100

    final changes to js file

--- sidebar-menu/app.js
+++ sidebar-menu/app.js
@@ -1,18 +1,18 @@
 // Variables
-let navExpand = document.querySelector('.nav__expand');
-let nav = document.querySelector('.nav');
-let navListItem = document.querySelectorAll('.nav__listitem');
+const navExpand = document.querySelector('.nav__expand');
+const nav = document.querySelector('.nav');
+const navListItem = document.querySelectorAll('.nav__listitem');
 
 // Functions
-function toggleNav() {
+const toggleNav = () => {
     nav.classList.toggle('nav-closed');
 };
 
-function listActive() {
+function listActive(e) {
     navListItem.forEach((link) => link.classList.remove("nav__listitem-active"));
     this.classList.add("nav__listitem-active");
   }
 
 // Event listeners
 navExpand.addEventListener('click', toggleNav);
-navListItem.forEach((link) => link.addEventListener('click', listActive));
+navListItem.forEach((link) => link.addEventListener("click", listActive));
